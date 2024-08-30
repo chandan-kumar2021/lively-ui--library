de7c8b2c87b4dd948d12daf0dc3227fa4848a31c
Chandan Kumar Mohanty <chandan@easyecom.io>
2024-08-30 12:04:23 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import MyComponent from "./lib/MyComponent.svelte";
+</script>
+
+<main>
+  <MyComponent text="Another instance of MyComponent" />
+</main>
+
+<style>
+</style>
---
+++ src/lib/MyComponent.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let text = "Default Text";
+</script>
+
+<div class="my-component">
+  {text}
+</div>
+
+<style>
+  .my-component {
+    padding: 10px;
+    background-color: lightblue;
+    border-radius: 5px;
+  }
+</style>
